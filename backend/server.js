7ffb115741d2384200575ea6200b6bd6fb520c77
llcain <llcain2012@gmail.com>
2023-04-23 18:05:58 -0500

    created backend folder - server.js file - installed Express

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for requests
+
+app.listen(8080, () => {
+        console.log("listening on port 8080!!");
+});
