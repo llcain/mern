9abf7dce8971e91eebc87d092c0eb5a0c276fc89
llcain <llcain2012@gmail.com>
2023-04-23 19:44:31 -0500

    created get rout in server.js file

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ const express = require('express');
 // express app
 const app = express();
 
+// routes
+app.get('/', (req,res) => {
+    res.json({mssg: 'Welcome to my app'});
+})
+
 // listen for requests
 
 app.listen(8080, () => {
-        console.log("listening on port 8080!!");
+        console.log("listening on port 8080 LaQuisha C.");
 });
